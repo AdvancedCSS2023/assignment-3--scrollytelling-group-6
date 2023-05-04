58d9969b6f3abe4522f681bc879a702a3242b482
MajaBru <122542734+MajaBru@users.noreply.github.com>
2023-05-04 18:15:36 +0200

    ocean svg scroll animation

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ let clouds = gsap.timeline();
 ScrollTrigger.create({
     animation: clouds,
     trigger: "img",
-    start: "top center",
+    start: "top 70%",
     end: "300%",
     scrub: 1,
-    markers: true,
 });
 
 clouds.to(".intro__cloud--one", { x: 1200 }, 0,)
@@ -17,6 +16,45 @@ clouds.to(".intro__cloud--two", { x: 900 }, 0)
 clouds.to(".intro__cloud--three", { x: -700 }, 0)
 clouds.to(".intro__cloud--four", { x:900 }, 0)
 clouds.to(".intro__cloud--five", { x: -1000 }, 0)
+
+
+gsap.to('.wave1', {
+    xPercent: -30,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".wave1",
+      start: "top center",
+      end: "bottom 40%",
+      scrub: 1,
+      markers: true,
+    }
+  })
+
+
+  gsap.to('.wave2', {
+    xPercent: -20,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".wave1",
+      start: "top center",
+      end: "bottom 40%",
+      scrub: 1,
+      markers: true,
+    }
+  })
+
+  
+gsap.to('.wave3', {
+    xPercent: -40,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".wave1",
+      start: "top center",
+      end: "bottom 40%",
+      scrub: 1,
+      markers: true,
+    }
+  })
 
 
 
