794e85f77e6484b1594f92375c9305194c4d763f
gudleik <johhaa@stud.ntnu.no>
2023-05-14 12:55:31 +0200

    johannes

--- script.js
+++ script.js
@@ -1,62 +1,84 @@
 gsap.registerPlugin(ScrollTrigger);
 
-
-// clouds moving on scroll
-let clouds = gsap.timeline();
-ScrollTrigger.create({
-    animation: clouds,
-    trigger: "img",
-    start: "top 70%",
-    end: "300%",
-    scrub: 1,
-});
-
-clouds.to(".intro__clouds--one", { x: 1200 }, 0,)
-clouds.to(".intro__clouds--two", { x: 900 }, 0)
-clouds.to(".intro__clouds--three", { x: 600 }, 0)
-clouds.to(".intro__clouds--four", { x: 500 }, 0)
-clouds.to(".intro__clouds--five", { x: -900 }, 0)
-
-
 // waves 
 
-gsap.to('.scene__wave--one', {
-    xPercent: -50,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".scene__wave--three",
-      start: "top 40%",
-      end: "+=700%", // changes duration by increasing the end point
-      scrub: 1,
-      markers: true,
-    }
-  })
-
-
-  gsap.to('.scene__wave--two', {
-    xPercent: -50,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".scene__wave--three",
-      start: "top 40%",
-      end: "+=700%",
-      scrub: 1,
-      markers: true,
-    }
-  })
-
-gsap.to('.scene__wave--three', {
-    xPercent: -50,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".scene__wave--three",
-      start: "top 40%",
-      end: "+=700%",
-      scrub: 1,
-      markers: true,
-    }
-  })
-
+gsap.to('.section1__wave--one', {
+  xPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section1__wave--three",
+    start: "top 40%",
+    end: "+=300%", // changes duration by increasing the end point
+    scrub: 1,
+    markers: true,
+  }
+})
+gsap.to('.section1__wave--two', {
+  xPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section1__wave--three",
+    start: "top 40%",
+    end: "+=300%",
+    scrub: 1,
+    markers: true,
+  }
+})
+gsap.to('.section1__wave--three', {
+  xPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section1__wave--three",
+    start: "top 40%",
+    end: "+=300%",
+    scrub: 1,
+    markers: true,
+  }
+})
+gsap.to('.section1__wave--four', {
+  xPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section1__wave--three",
+    start: "top 40%",
+    end: "+=300%",
+    scrub: 1,
+    markers: true,
+  }
+})
+gsap.to('.man', {
+  x: "50vw",
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section1__wave--three",
+    start: "top 40%",
+    end: "+=100%",
+    scrub: 1,
+    markers: true,
+  }
+})
+gsap.to('.section1__beach', {
+  x: "-50vw",
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section1__wave--three",
+    start: "top",
+    end: "+=100%",
+    scrub: 1,
+    markers: true,
+  }
+})
+gsap.to('.beach2', {
+  x: "-80vw",
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section2",
+    start: "bottom",
+    end: "+=200%",
+    scrub: 1,
+    markers: true,
+  }
+})
 
 
 //reload reset
